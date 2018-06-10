dc855dd92e6d4a77d81cc178ddfe674691eabbf7
msandim <migsandim@hotmail.com>
2018-06-10 22:07:47 +0100

    parsing cmd arguments done (with error messages)

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net/url"
+	"os"
 	"webcrawler/crawler"
 )
 
@@ -12,7 +14,20 @@ func parseArguments() (nWorkers int, rateLimit int, domain string) {
 	flag.StringVar(&domain, "domain", "", "the domain to crawl")
 	flag.Parse()
 
-	// TODO: check if the domain is valid, and the ints > 0
+	if !isnWorkersValid(nWorkers) {
+		fmt.Fprintln(os.Stderr, "main::parseArguments() - Error: Number of workers is invalid: ", nWorkers)
+		os.Exit(-1)
+	}
+
+	if !isRateLimitValid(rateLimit) {
+		fmt.Fprintln(os.Stderr, "main::parseArguments() - Error: Rate limit is invalid: ", rateLimit)
+		os.Exit(-1)
+	}
+
+	if !isDomainValid(domain) {
+		fmt.Fprintln(os.Stderr, "main::parseArguments() - Error: Domain is invalid: ", domain)
+		os.Exit(-1)
+	}
 	return
 }
 
@@ -22,4 +37,25 @@ func main() {
 	fmt.Println("nworkers: ", nWorkers, " ratelimit: ", rateLimit, " domain: ", domain)
 	crawler := crawler.New(nWorkers, rateLimit, domain)
 	crawler.Run()
+}
+
+func isnWorkersValid(nWorkers int) bool {
+	return nWorkers > 0
+}
+
+func isRateLimitValid(rateLimit int) bool {
+	return rateLimit > 0
+}
+
+func isDomainValid(domain string) bool {
+	domainParsed, err := url.Parse(domain)
+	if err != nil {
+		return false
+	}
+
+	if domainParsed.Scheme == "" || domainParsed.Hostname() == "" || domainParsed.Path == "" {
+		return false
+	}
+
+	return true
 }
